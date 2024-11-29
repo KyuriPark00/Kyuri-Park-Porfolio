02ab75d460e5475972857462261f3837dcb718b3
KyuriPark00 <156356426+KyuriPark00@users.noreply.github.com>
2024-11-28 19:42:18 -0500

    Update index.html

--- js/main.js
+++ js/main.js
@@ -27,4 +27,25 @@ toggleButton.addEventListener("click", () => {
       toggleButtonIcon.classList.remove("fa-solid", "fa-bars");
       toggleButtonIcon.classList.add("fa-regular", "fa-circle-xmark");
     }
+    
   });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const player = new Plyr(".player", {
+      controls: [
+        "play-large", // 중앙의 큰 재생 버튼
+        "play",       // 재생/일시정지 버튼
+        "progress",   // 진행 바
+        "current-time", // 현재 재생 시간
+        "mute",       // 음소거 버튼
+        "volume",     // 볼륨 조절
+        "settings",   // 설정 옵션
+        "fullscreen", // 전체 화면 버튼
+      ],
+      settings: ["captions", "quality", "speed"], // 설정 옵션
+      autoplay: false, // 자동 재생 비활성화
+      volume: 0.8, // 초기 볼륨 설정
+    });
+  });
+
+  
