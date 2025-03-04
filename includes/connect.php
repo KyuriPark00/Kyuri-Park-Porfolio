fc10a860482d34e9d0a45098f1443a56395d94b9
KyuriPark00 <156356426+KyuriPark00@users.noreply.github.com>
2025-03-04 15:12:36 -0500

    Update

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <!-- opperation가서 collation 바꾸기 -->
 <!-- 도메인에서 정해준 데이터베이스이름, 유저이름, 패스워드(아마 없을수도) 로 바꿔야함함 -->
 <?php
-$dsn = "mysql:host=localhost;dbname=newportfolio;charset=utf8mb4";
+$dsn = "mysql:host=localhost;dbname=afgmsu91_portfolio;charset=utf8";
 
 try {
 $connection = new PDO($dsn, 'root', '');
